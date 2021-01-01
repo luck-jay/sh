e2a99597c6606178074c0769dcabe20e434cf4a0
yunjie526 <a1351090507@163.com>
2021-01-01 16:40:17 +0800

    优化触控板设置脚本

--- touchpad.sh
+++ touchpad.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-# 获取触摸板信息
-DATA=$(xinput list | grep Touchpad)
+# 获取触摸板信息,如果没有获取到触控板则推出这个脚本
+DATA=$(xinput list | grep Touchpad) || exit 1
 DATA=${DATA##*=}
 touchpad=${DATA%%\[*}
+# 获取需要设置的属性id
+DATA=$(xinput list-prop $touchpad | grep -v Default | grep 'Tapping Enabled')
+DATA=${DATA##*\(}
+# 获取到设置触控板点击有效的id
+TAP_EN_ID=${DATA%%)*}
+
+DATA=$(xinput list-prop $touchpad | grep -v Default | grep 'Scrolling Enabled')
+DATA=${DATA##*\(}
+# 获取到设置触控板自然滚动的id
+SCR_EN_ID=${DATA%%)*}
 # 设置触摸板
-xinput set-prop $touchpad 334 1
-xinput set-prop $touchpad 314 1
+xinput set-prop $touchpad $TAP_EN_ID 1
+xinput set-prop $touchpad $SCR_EN_ID 1
