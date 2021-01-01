d2e3972d17b07f46676905aa4e76c089213a0e5d
yunjie526 <a1351090507@163.com>
2021-01-01 21:20:44 +0800

    修改一些简略信息

--- autoDwm.sh
+++ autoDwm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # 启动窗口渲染器
-compton
+compton &
 # 加载键盘布局
 ~/sh/setXmodmap.sh &
 # 显示系统时间
-~/sh/printDate.sh &
+~/sh/printInfo.sh &
 # 配置壁纸启动
 nitrogen --restore
 # 配置扩展屏显示
-xrandr --output eDP1 --right-of HDMI1 --auto
+xrandr --output eDP-1 --right-of HDMI-1 --auto
 #feh --randomize --bg-fill ~/image
 # 配置java应用程序会出现的问题
 wmname LG3D
