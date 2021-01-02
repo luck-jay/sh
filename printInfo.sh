240d5c004a9b1d3fa9703ed0d1952cd6962fd4e3
yunjie526 <a1351090507@163.com>
2021-01-02 22:19:47 +0800

    添加了输出网络信息的功能

--- printInfo.sh
+++ printInfo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ELECTRICITY="0%"
 VOLUME="0%"
+NET=""
 # 更新当前电池信息
 function getacpi(){
 	DATA=$(acpi)
@@ -16,10 +17,26 @@ function getvolume(){
 	DATA=${DATA#*[}
 	VOLUME=${DATA%%]*}
 }
+# 更新网络状态
+function getinernet(){
+	INTER=$(ip addr | grep -v lo | grep LOWER_UP)
+	if [ -z "$INTER" ]; then
+		NET="NO"
+	else
+		if [ ${INTER:3:1} = "e" ]; then
+			NET="NET"
+		elif [ ${INTER:3:1} = "w" ]; then
+			NET="WIFI"
+		else
+			NET="WIFI"
+		fi
+	fi
+}
 # 每过1秒刷新一次时间
 while true;do
 	getacpi
 	getvolume
-	xsetroot -name "VOL | $VOLUME | BAT | $ELECTRICITY | TIME $(date "+%Y-%m-%d %H:%M:%S")"
+	getinernet
+	xsetroot -name "NET | $NET |VOL | $VOLUME | BAT | $ELECTRICITY | TIME $(date "+%Y-%m-%d %H:%M:%S")"
 	sleep 1s
 done
