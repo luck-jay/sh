030d8a76884eb2b26b118762593cfa8f62518f57
yunjie526 <a1351090507@163.com>
2021-01-02 14:28:32 +0800

    修改了一个错误

--- touchpad.sh
+++ touchpad.sh
@@ -4,12 +4,12 @@ DATA=$(xinput list | grep Touchpad) || exit 1
 DATA=${DATA##*=}
 touchpad=${DATA%%\[*}
 # 获取需要设置的属性id
-DATA=$(xinput list-prop $touchpad | grep -v Default | grep 'Tapping Enabled')
+DATA=$(xinput list-props $touchpad | grep -v Default | grep 'Tapping Enabled')
 DATA=${DATA##*\(}
 # 获取到设置触控板点击有效的id
 TAP_EN_ID=${DATA%%)*}
 
-DATA=$(xinput list-prop $touchpad | grep -v Default | grep 'Scrolling Enabled')
+DATA=$(xinput list-props $touchpad | grep -v Default | grep 'Scrolling Enabled')
 DATA=${DATA##*\(}
 # 获取到设置触控板自然滚动的id
 SCR_EN_ID=${DATA%%)*}
