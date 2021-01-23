23bfb42a7fc5d8c1d2000d013fdd1b808e5a778f
yunjie526 <a1351090507@163.com>
2021-01-23 17:13:07 +0800

    简单的修正

--- autoDwm.sh
+++ autoDwm.sh
@@ -2,15 +2,15 @@
 # 启动窗口渲染器
 compton &
 # 加载键盘布局
-~/sh/setXmodmap.sh &
-# 显示系统时间
-~/sh/printInfo.sh &
+$HOME/sh/setXmodmap.sh &
+# 显示系统信息
+$HOME/sh/printInfo.sh &
 # 配置壁纸启动
 nitrogen --restore
 # 配置扩展屏显示
-xrandr --output eDP-1 --right-of HDMI-1 --auto
-#feh --randomize --bg-fill ~/image
+# xrandr --output eDP-1 --right-of HDMI-1 --auto
+# feh --randomize --bg-fill ~/image
 # 配置java应用程序会出现的问题
 wmname LG3D
 # 配置触摸板
-~/sh/touchpad.sh &
+$HOME/sh/touchpad.sh &
