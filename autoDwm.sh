fd9975c45970ffe168700401744683e010030d61
yunjie526 <a1351090507@163.com>
2021-03-07 13:00:23 +0800

    添加启动输入法

--- autoDwm.sh
+++ autoDwm.sh
@@ -8,9 +8,11 @@ $HOME/sh/printInfo.sh &
 # 配置壁纸启动
 nitrogen --restore
 # 配置扩展屏显示
-# xrandr --output eDP-1 --right-of HDMI-1 --auto
+ xrandr --output eDP-1 --right-of HDMI-1 --auto
 # feh --randomize --bg-fill ~/image
 # 配置java应用程序会出现的问题
 wmname LG3D
 # 配置触摸板
 $HOME/sh/touchpad.sh &
+# 配置输入法启动
+fcitx &
